937ab3bfb05a296706351313f47c0bd400f6fd8a
cybomax <104077965+cybomax@users.noreply.github.com>
2022-04-20 15:07:20 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Vector.h"
+
+using Value = double;
+
+int main()
+{
+    Value arr1[3] = {1.2, 9.68, 7.89};
+    Value arr2[4] = {21.8, 91.2, 3.53, 2.13};
+    Vector a(arr1, 3, 2);
+    std::cout << a[0] << std::endl;
+    std::cout << a.size() << std::endl;
+    std::cout << a.capacity() << std::endl;
+
+    Vector b;
+    b.pushBack(57.9897);
+    b.pushFront(1.45);
+    b.pushBack(347.9897);
+    b.pushBack(14.9897);
+    std::cout << b[0] << std::endl;
+    std::cout << b.size() << std::endl;
+    std::cout << b.capacity() << std::endl;
+
+    Vector c;
+    c = std::move(a);
+    c.insert(b, 2);
+    c.popBack();
+    c.popFront();
+    c.eraseBetween(2, 5);
+    std::cout << c[0] << std::endl;
+    std::cout << c[1] << std::endl;
+    c[0] = 5;
+    std::cout << c.find(8) << std::endl;
+    c.shrinkToFit();
+    std::cout << c.size() << std::endl;
+    std::cout << c.capacity() << std::endl;
+    return 0;
+}
